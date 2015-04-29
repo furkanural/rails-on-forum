fcfcda3e0262af1f31141056254cb92fdbceea32
Furkan URAL <uralfrkn@gmail.com>
2015-04-29 22:38:16 +0300

    hesapların olusturulması saglandı

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :users
+  get 'users/new', to: redirect('/kaydol')
+  resources :users, expect: :index
+  get '/kaydol', to: 'users#new', as: :register
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
