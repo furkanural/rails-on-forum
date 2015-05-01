2a7416575d7cce73a749e71fd5e9aa111f8a5a48
Furkan URAL <uralfrkn@gmail.com>
2015-05-01 23:09:15 +0300

    Forum modeli oluşturuldu ve başlangıc kayıtları girildi

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150429182224) do
+ActiveRecord::Schema.define(version: 20150501195247) do
+
+  create_table "forums", force: :cascade do |t|
+    t.string "name", null: false
+  end
+
+  add_index "forums", ["name"], name: "index_forums_on_name", unique: true
 
   create_table "users", force: :cascade do |t|
     t.string   "username",        null: false
