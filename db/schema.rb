a3870fb6a31e7a532454a4f7ba4faac78e783043
uralfrkn <uralfrkn@gmail.com>
2015-06-03 23:04:42 +0300

    sorgular optimize edildi

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150509121442) do
+ActiveRecord::Schema.define(version: 20150603195332) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body",       null: false
@@ -31,12 +31,13 @@ ActiveRecord::Schema.define(version: 20150509121442) do
   add_index "forums", ["name"], name: "index_forums_on_name", unique: true
 
   create_table "topics", force: :cascade do |t|
-    t.string   "title",      null: false
-    t.text     "body",       null: false
+    t.string   "title",                      null: false
+    t.text     "body",                       null: false
     t.integer  "user_id"
     t.integer  "forum_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.integer  "comments_count", default: 0
   end
 
   add_index "topics", ["forum_id"], name: "index_topics_on_forum_id"
